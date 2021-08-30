aee00686c9132b40e4e6f04c8298a25db3937a16
eethann <ethan@colab.coop>
2021-08-30 11:23:19 -0400

    Fix projects attribute reference

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { attributes, react as HomeContent } from '../content/home.md';
 
 export default class Home extends Component {
   render() {
-    let { title, cats } = attributes;
+    let { title, projects } = attributes;
     return (
       <>
         <Head>
