b07889cf68bf21fddfa098ed80abce65559575b9
Tomaz Korenika <tomazkorenika@Tomazs-Mac-mini.local>
2021-09-05 10:14:04 +0200

    remove some packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended",
-        "next"
+        "plugin:react/recommended"
     ],
     "parserOptions": {
         "ecmaFeatures": {
