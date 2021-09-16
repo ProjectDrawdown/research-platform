70f9be1c48adea6e5c0dc3471cdd8db18dda5984
chukslord1 <chukslord1@gmail.com>
2021-09-16 03:22:11 +0100

    changed component name'

--- pages/projects/browseProjects.js
+++ pages/projects/browseProjects.js
@@ -3,7 +3,7 @@ import Head from "next/head"
 import { Component } from 'react'
 import { ChakraProvider, Flex, Heading, Text, Button, Stack, HStack, Grid, Box, Link, Input} from "@chakra-ui/react"
 
-export default class Home extends Component {
+export default class BrowseProjects extends Component {
   render() {
     return (
       <>
