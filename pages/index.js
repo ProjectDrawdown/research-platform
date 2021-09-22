4e2abffe34585e620bda84dbf17f1da0bc5fbe73
chukslord1 <chukslord1@gmail.com>
2021-09-22 19:31:36 +0100

    added resposiveness

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import React, {useState} from "react"
 import Head from "next/head"
 import { ChakraProvider, Flex, Link, Button, Heading, Text, Stack, HStack, Image, Grid, SimpleGrid, Box} from "@chakra-ui/react"
+import { createBreakpoints } from "@chakra-ui/theme-tools"
 
+// This is the default breakpoint
+const breakpoints = createBreakpoints({
+sm: "30em",
+md: "48em",
+lg: "62em"
+})
 
 const Header = () =>{
   return (
     <>
-      <Box top="0px" left="0px" width="100" height="max-content">
+      <Box top="0px" left="0px" width="100%" height="max-content">
         <Box background="#FFFFFF"
-        h="621px"
-        w="792px"
-        left="28px"
+        w={["80%", "60%", "50%"]}
+        left="10%"
         top="53px"
         zIndex="100"
         border="4px solid #000000"
@@ -24,22 +30,19 @@ const Header = () =>{
         borderRadius="10px"
         borderStyle="solid"
         position="absolute">
-        <Heading as="h1" textStyle="caps" fontSize="48px" height="96px" fontFamily="Sora" 
-        weight="800px" left="78px" top="93px" textAlign="left" paddingRight="25%">
+        <Heading as="h1" textStyle="caps" fontSize={["20px", "30px", "48px"]} fontFamily="Sora" left="78px" top="93px" textAlign="left" paddingRight={["10%", "25%"]}>
           Open Solutions Collaboratory
         </Heading>
-          <HStack >
-            <Text fontSize="30px" fontWeight="600px" paddingTop="2rem" paddingRight="35%"  textAlign="left" left="78px" top="243px"> 
-            Building the worlds  largest open research platform for climate solutions...
-            </Text>
-          </HStack>
+          <Text fontSize={["15px", "15px", "30px"]} fontWeight="600px" paddingTop="2rem" paddingRight={["10%", "35%"]}  textAlign="left" > 
+          Building the worlds  largest open research platform for climate solutions...
+          </Text>
         </Box>
         <Box position="relative" 
         border="4px solid #000000"
-        width = "860px"
-        height= "800px"
-        left = "452px"
-        top = "150px" 
+        h={["250px", "250px", "500px"]}
+        w={["60%", "50%"]}
+        left = {["40%"]}
+        top = {["150px" ]}
         zIndex="90"
         marginBottom="150px"
         background = "url(https://bit.ly/sage-adebayo)"
@@ -59,7 +62,7 @@ const FeaturedProjectsList = () =>{
   
   return (
     <>
-      <Box paddingRight="60%" paddingBottom="3em" paddingTop="3em">
+      <Box paddingBottom="3em" paddingTop="3em">
         <Text fontSize="36px" fontWeight="bold" textStyle="caps" textAlign="left">
           Recently Active Projects
         </Text>
@@ -68,9 +71,9 @@ const FeaturedProjectsList = () =>{
       <Grid templateColumns="1fr" gap={6} background="url(https://bit.ly/sage-adebayo)" backgroundPosition="center" backgroundSize="cover">
         <Grid templateColumns="repeat(2, 1fr)" gap="20px" >
           {projects.slice(0,2).map(project => (
-          <Box w="100%" padding="45" position="relative" background="#F5F5F5" margin="auto"  key={project}>
-            <Text boxSize="400px" >
-              <Heading as="h2" size="md" textStyle="caps" fontSize="30px" color="#00C24E">
+          <Box w="100%" minHeight={["200px", "200px","300px"]} padding="45px 10px" position="relative" background="#F5F5F5" margin="auto"  key={project}>
+            <Text boxSize="100%" >
+              <Heading as="h2" size="md" textStyle="caps" fontSize={["15px", "20px", "30px"]} color="#00C24E">
                 {project.name}
               </Heading>
             </Text>
@@ -87,9 +90,9 @@ const FeaturedProjectsList = () =>{
         </Heading>
         <Grid templateColumns="repeat(2, 1fr)" gap="20px">
           {projects.slice(2,4).map(project => (
-          <Box w="100%" padding="45" position="relative" background="#F5F5F5" margin="auto"  key={project}>
-            <Text boxSize="400px" >
-              <Heading as="h2" size="md" textStyle="caps" fontSize="30px" color="#00C24E">
+          <Box w="100%" minHeight={["200px", "200px","300px"]} padding="45px 10px" position="relative" background="#F5F5F5" margin="auto"  key={project}>
+            <Text boxSize="100%" >
+              <Heading as="h2" size="md" textStyle="caps" fontSize={["15px", "20px", "30px"]} color="#00C24E">
                 {project.name}
               </Heading>
             </Text>
@@ -108,19 +111,19 @@ const FeaturedProjectsList = () =>{
 const Partners = () =>{
   return(
     <>
-      <Box paddingRight="60%" paddingBottom="3em" paddingTop="3em">
+      <Box paddingBottom="3em" paddingTop="3em">
         <Text fontSize="36px" fontWeight="bold" textStyle="caps" textAlign="left">
           Our Partners
         </Text>
       </Box>
-      <SimpleGrid minChildWidth="370px" spacing="40px" width="100%">
-        <Image borderRadius="full" maxWidth="360px" src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
-        <Image borderRadius="full" maxWidth="229px" src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
-        <Image borderRadius="full" maxWidth="218px" src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
-        <Image borderRadius="full" maxWidth="370px" src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
-        <Image borderRadius="full" maxWidth="370px" src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
-        <Image borderRadius="full" maxWidth="370px" src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
-        <Image borderRadius="full" maxWidth="370px" src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+      <SimpleGrid minChildWidth="370px" padding="50px" spacing="40px" width="100%">
+        <Image borderRadius="full" maxWidth={["250px", "250px", "360px"]} src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+        <Image borderRadius="full" maxWidth={["250px", "250px", "360px"]} src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+        <Image borderRadius="full" maxWidth={["250px", "250px", "360px"]} src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+        <Image borderRadius="full" maxWidth={["250px", "250px", "360px"]} src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+        <Image borderRadius="full" maxWidth={["250px", "250px", "360px"]} src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+        <Image borderRadius="full" maxWidth={["250px", "250px", "360px"]} src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+        <Image borderRadius="full" maxWidth={["250px", "250px", "360px"]} src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
       </SimpleGrid>
     </>
   )
@@ -129,12 +132,12 @@ const Partners = () =>{
 const Body = () =>{
   return (
     <>
-      <Box paddingRight="60%"  paddingBottom="3em" paddingTop="3em">
+      <Box paddingBottom="3em" paddingTop="3em">
           <Text fontSize="36px" fontWeight="bold" textStyle="caps" textAlign="left">
             Who We Are
           </Text>
       </Box>
-      <Grid templateColumns="1fr 1.5fr" width="40%" gap={6} padding="40px" background="url(https://bit.ly/sage-adebayo)" backgroundPosition="center" backgroundSize="cover">
+      <Grid templateColumns="1fr 1.5fr" width={["100", "70%", "50%"]} gap={6} padding="40px" background="url(https://bit.ly/sage-adebayo)" backgroundPosition="center" backgroundSize="cover">
         <Stack marginLeft="-40%" gap={6}>
           <Box textAlign="center" padding="20px" paddingTop="0px" minWidth="200px" maxWidth="300px" border="2px solid #000000" borderRadius="5px" background="#FFFFFF">
             <Heading as="h2" mt="5" size="md"> 12 </Heading>
@@ -149,7 +152,7 @@ const Body = () =>{
             <Heading as="h4" mt="5" size="md" color="blue"> Projects </Heading>
           </Box>
         </Stack>
-          <Box padding="20px" maxWidth="300px" border="2px solid #000000" borderRadius="5px" background="#FFFFFF">
+          <Box padding="20px" maxWidth={["100%"]} border="2px solid #000000" borderRadius="5px" background="#FFFFFF">
             <Text fontWeight="bold">
               There could be a short description here with a bit more inspirational somethn’ somethn’
             </Text>
@@ -200,12 +203,15 @@ const Body = () =>{
 
 const Footer = () =>{
   return (
-    <>
+    <Box
+      w="100% "
+      position="relative"
+    >
       <Box background = "url(https://bit.ly/sage-adebayo)"
-        h="400px"
-        w="800px"
+        h={["200px", "250px", "300px"]}
+        w={["50%", "50%", "60%"]}
         left="30px"
-        top="4726px"
+        bottom="100px"
         zIndex="100"
         border="4px solid #000000"
         p="3"
@@ -220,10 +226,11 @@ const Footer = () =>{
           <Box
           background="#000000"
           h="237px"
-          w="520px"
-          left="775px"
-          top="4899px"
+          w={["70%", "50%", "50%"]}
+          left={["40%", "40%", "40%"]}
+          bottom="50px"
           zIndex="100"
+          marginTop={["200px", "150px", "200px"]}
           border="4px solid #000000"
           p="3"
           boxShadow="base"
@@ -233,22 +240,19 @@ const Footer = () =>{
           borderColor="black"
           borderWidth="3px"
           borderStyle="solid"
-          position="absolute"
+          position="relative"
           >
-        <Heading as="h1" textStyle="caps" fontSize="36px" height="80px" fontFamily="Sora" 
-        weight="402px" left="807px" top="93px" textAlign="left" paddingRight="25%" color="#FFF">
+        <Heading as="h1" textStyle="caps" fontSize={["20px", "28px", "36px"]} fontFamily="Sora" textAlign="left" paddingRight="15%" color="#FFF">
           Open Solutions Collaboratory
         </Heading>
-        <Text textStyle="caps" fontSize="18px" height="96px" fontFamily="Sora" 
-        weight="600" left="807px" top="5029px" textAlign="left" paddingRight="25%" color="#FFF">
+        <Text textStyle="caps" fontSize={["12px", "14px", "18px"]} fontFamily="Sora" textAlign="left" paddingRight="15%" color="#FFF">
           Contact Us
         </Text>
-        <Text textStyle="caps" fontSize="14px" height="20px" fontFamily="Sora" 
-        weight="800" left="807px" top="5093px" textAlign="left" paddingRight="25%" color="#FFF">
+        <Text textStyle="caps" fontSize="14px" fontFamily="Sora" textAlign="left" paddingRight="15%" color="#FFF">
           ©2021
         </Text>
       </Box>
-  </>
+  </Box>
   )
 }
 
