4836151b08fae00a6350ebff345bfb114e3efe45
chukslord1 <chukslord1@gmail.com>
2021-09-30 05:09:47 +0100

    restyled decoupled compnents

--- components/BackButton.js
+++ components/BackButton.js
@@ -5,9 +5,7 @@ import { Button, Link, Text } from "@chakra-ui/react"
 export default function BackButton () {
     return (
       <Link href="/">
-        <Button background="#ffffff">
-          <Text fontSize="14px">{'<<'} Back to Home </Text>
-        </Button>
+        <Text fontSize="14px">{'<<'} Back to Home </Text>
       </Link>
     )
 }
--- components/StyledButton.js
+++ components/StyledButton.js
@@ -4,7 +4,7 @@ import { Button } from "@chakra-ui/react"
 
 export default function StyledButton ({content}) {
     return (
-      <Button colorScheme="yellow" borderRadius="0px" border="2px solid #000000">
+      <Button bg="#F1FF51" borderRadius="0px" marginX="auto" border="2px solid #000000" >
           {content}
       </Button>
     )
