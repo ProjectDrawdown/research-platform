33d2642fc28b6b8b77bf0467b0955968e4fd1217
chukslord1 <chukslord1@gmail.com>
2021-09-30 04:57:30 +0100

    added base page link

--- components/BackButton.js
+++ components/BackButton.js
@@ -4,7 +4,7 @@ import { Button } from "@chakra-ui/react"
 
 export default function BackButton () {
     return (
-      <Link href="browseProjects">
+      <Link href="/">
         <Button background="#ffffff">
           <Text fontSize="14px">{'<<'} Back to Home </Text>
         </Button>
