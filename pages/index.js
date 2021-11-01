5aec581096a54948c620e32332c262b9ced83910
David Brooks <david@colab.coop>
2021-11-01 09:44:26 -0500

    testing form on root

--- pages/index.js
+++ pages/index.js
@@ -246,6 +246,42 @@ const Home = () => {
       <AboutUs project_image={project_image} />
       <SneakPeek />
       <Footer bottom_image={bottom_image} />
+
+          <form name="contact" method="POST" data-netlify="true">
+            <Box marginTop="2rem">
+              <label>Your Name:
+                <Input borderColor="black"
+                       size="md"
+                       type="text"
+                       name="name" />
+              </label>
+            </Box>
+            <Box marginTop="2rem">
+              <label>Your Email:
+                <Input borderColor="black"
+                       size="md"
+                       type="email"
+                       name="email" />
+              </label>
+            </Box>
+            <Box marginTop="2rem">
+              <label>Message:
+                <Textarea borderColor="black"
+                          size="md"
+                          name="message">
+                </Textarea>
+              </label>
+            </Box>
+
+            <Box marginTop="1rem">
+              <Button bg="#F1FF51" borderRadius="0px" marginX="auto" border="2px solid #000000" padding={{base: "15px", "2xl":"50px"}} type="submit">
+                <Text fontSize={{base:"15px", sm:"15px", md:"15pxpx", lg:"15px", xl:"15px", "2xl":"50px"}}>
+                  SEND
+                </Text>
+              </Button>
+            </Box>
+          </form>
+
   </div>
   )
 }
