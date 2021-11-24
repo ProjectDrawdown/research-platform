39a478ecb84c75ff1649173ce79244245be0d0bd
Tomaž Korenika <litenull@gmail.com>
2021-11-24 10:20:51 +0100

    menu things

--- components/Menu.js
+++ components/Menu.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link, Text, Grid, GridItem } from "@chakra-ui/react"
+import { FcHome } from "react-icons/fc"
 
 
 export default function Menu () {
   return (
-    <Grid templateColumns="repeat(2, 1fr)" textAlign="center" w="30vw">
-      <GridItem borderRight="1px solid #000">
+    <Grid templateColumns="repeat(2, 1fr)" textAlign="center" w="19rem">
+      <GridItem borderRight="2px solid #000">
         <Link href="/">
-          <Text fontSize="1rem">Home</Text>
+          <FcHome size={30} style={{ margin: "auto" }} />
         </Link>
       </GridItem>
       <GridItem>
